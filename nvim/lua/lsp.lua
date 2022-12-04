e5385ac4907bc615e95473acb407368651baab16
Onur Kader <onurorkunkader1999@gmail.com>
2022-12-04 16:31:56 +0300

    Random: Some xdg-ninja stuff, some cursor things, and some nvim-lsp fixes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -113,7 +113,7 @@ cmp.event:on(
 -- })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 --[[
@@ -359,10 +359,10 @@ lspconfig["tsserver"].setup({
 	on_attach = on_attach,
 })
 
-lspconfig["bashls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
+-- lspconfig["bashls"].setup({
+-- 	capabilities = capabilities,
+-- 	on_attach = on_attach,
+-- })
 
 lspconfig["sumneko_lua"].setup({
 	capabilities = capabilities,
