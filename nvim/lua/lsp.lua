652c187ba9c0679cee5a5bf3bfcb940f56cdf293
Beron the Colossus <onurorkunkader1999@gmail.com>
2024-05-07 13:46:57 +0300

    nvim/lsp: Slightly modify config and add ruff
    > Also switch to none-ls

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -18,6 +18,35 @@ cmp.setup({
 			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
 	completion = {
 		keyword_length = 2,
 	},
@@ -34,15 +63,12 @@ cmp.setup({
 		}),
 	},
 	mapping = {
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
@@ -62,7 +88,6 @@ cmp.setup({
 				feedkey("<Plug>(vsnip-jump-prev)", "")
 			end
 		end, { "i", "s" }),
-		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lua" },
@@ -426,24 +451,30 @@ lspconfig["vls"].setup({
 	on_attach = on_attach,
 })
 
+lspconfig["ruff_lsp"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
 local nl = require("null-ls")
 local nl_builtins = nl.builtins
+
+--flake8, rustfmt
+local shchk = require("none-ls-shellcheck")
 
 nl.setup({
 	on_attach = on_attach,
 	sources = {
-		nl_builtins.code_actions.shellcheck,
+		shchk.diagnostics,
+		shchk.code_actions,
 		-- nl_builtins.diagnostics.clang_check,
 		-- So annoying, it's checking headers, and reporting them in the source file but obviously the location is wrong
 		-- nl_builtins.diagnostics.cppcheck.with({ "-j4", "--std=c++23", "--suppress='*:source/*.h'", "--suppress='*:source/*.hpp'", "--suppress='*:src/*.hpp'", "--suppress='*:src/*.h'", "--suppress='*:**/*.hpp'", "--suppress='*:**/*.h'", "--suppress='*:include/*.h'", "--suppress='*:include/*.hpp'", "--suppress='*:*.hpp'", "--suppress='*:*.h'", "--enable=warning,style,performance,portability,information,missingInclude",
 		-- 	"--template=gcc", "$FILENAME" }),
-		nl_builtins.diagnostics.shellcheck,
 		nl_builtins.diagnostics.zsh.with({ method = nl.methods.DIAGNOSTICS_ON_SAVE }),
 		nl_builtins.formatting.clang_format,
 		nl_builtins.formatting.prettier,
-		nl_builtins.diagnostics.flake8,
 		nl_builtins.formatting.black,
-		nl_builtins.formatting.rustfmt,
 		nl_builtins.formatting.shfmt,
 		nl_builtins.formatting.stylua,
 	},
